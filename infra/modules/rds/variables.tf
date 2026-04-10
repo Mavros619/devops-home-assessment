d1e12b6b4dbbbb44f02727d0b6d36bc65c76339a
Mavros619 <ammavros619@gmail.com>
2026-04-10 18:15:38 +0300

    Add RDS module variables for database configuration

---
+++ infra/modules/rds/variables.tf
@@ -0,0 +1,45 @@
+variable "db_name" {
+  description = "The name of the database to create"
+  type        = string
+  default     = "production_db"
+}
+
+variable "db_username" {
+  description = "Master username for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Master password for the database"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t3.medium"
+}
+
+variable "db_allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = number
+  default     = 20
+}
+
+variable "db_multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ (Required for High Availability in Prod)"
+  type        = bool
+  default     = true
+}
+
+variable "db_subnet_group_name" {
+  description = "Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group."
+  type        = string
+}
+
+variable "db_security_group_ids" {
+  description = "List of VPC security groups to associate"
+  type        = list(string)
+}
